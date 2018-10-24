74c758fdb68bc03eaf1b6c1ddbe6e25619f7b8bc
Productive Programmer <charles.lobo@gmail.com>
2018-10-25 08:37:08 +1100

    Check that owner is messaging telegram before replying

--- communicator.js
+++ communicator.js
@@ -14,10 +14,87 @@ const client = new cote.Requester({
 let TELEGRAM;
 
 /*      outcome/
- * Save the telegram client so we can use it asynchronously
+ * Save the telegram client so we can use it asynchronously and load the
+ * telegram owner if needed.
  */
 function setup(tg) {
     TELEGRAM = tg
+    if(!owner.loaded) loadOwner()
+}
+
+/*      problem/
+ * As we are a 'bot' any user can find us and talk with us and get us to
+ * do their bidding. Not just our owner. This is not good.
+ *
+ *      way/
+ * Check if this is our owner and only then respond.
+ */
+function handleMsg(ctx) {
+    isOwner(ctx, (err, isowner) => {
+        if(err) {
+            u.showErr(err)
+            if(typeof err === 'string') ctx.reply(err)
+            else ctx.reply('Error!')
+            return
+        }
+        if(isowner) handleOwnerMsg(ctx)
+        else ctx.reply('Error! I only respond to my owner')
+    })
+}
+
+
+const ssb = new cote.Requester({
+    name: 'elife-telegram -> ssb',
+    key: 'everlife-ssb-svc',
+})
+
+let owner = {
+    loaded: false,
+    id: null,
+}
+function loadOwner() {
+    ssb.send({
+        type: 'msg-by-type',
+        mstype: 'elife-telegram.owner',
+    }, (err, msgs) => {
+        if(err) {
+            u.showErr(err)
+            setTimeout(loadOwner, 10000)
+        } else {
+            owner.loaded = true
+            if(msgs.length) owner.id = msgs[0].val
+        }
+    })
+}
+
+/*      outcome/
+ * When we get the first message - because we don't have any context, we
+ * assume it is our owner and 'bind' to it. From that point onward we
+ * only accept messages from this user.
+ */
+function isOwner(ctx, cb) {
+    if(!owner.loaded) cb(`Error! Data not loaded. Not ready`)
+    else {
+        if(!owner.id) bind_user_1(ctx, cb)
+        else cb(null, owner.id == ctx.from.id)
+    }
+
+    function bind_user_1(ctx, cb) {
+        owner.id = ctx.from.id
+        if(!owner.id) cb(`Error! Telegram owner not found`)
+        else {
+            ssb.send({
+                type: 'new-msg',
+                msg: {
+                    type: 'elife-telegram.owner',
+                    val: owner.id,
+                },
+            }, (err) => {
+                if(err) cb(err)
+                else cb(null, true)
+            })
+        }
+    }
 }
 
 /*      outcome/
@@ -26,7 +103,7 @@ function setup(tg) {
  * In the special case that it is a `start` message, respond with
  * welcoming the user.
  */
-function handleMsg(ctx) {
+function handleOwnerMsg(ctx) {
     if(!TELEGRAM) {
         ctx.reply('Error! Telegram not registered with Avatar')
         return
